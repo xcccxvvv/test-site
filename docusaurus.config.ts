c7f90a1387e40bc5f911abf0f0b2006de2eb8b21
张方生 <fangsheng.zhang@igen-tech.com>
2025-08-08 10:28:14 +0800

    Enable Docusaurus experimental_faster mode and add @docusaurus/faster

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,6 +13,7 @@ const config: Config = {
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
+    experimental_faster: true,
   },
 
   // Set the production url of your site here
@@ -28,7 +29,7 @@ const config: Config = {
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
-  plugins: ["./plugins/tailwind-plugin.cjs"],
+  plugins: [["./plugins/tailwind-plugin.cjs", {}]],
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
